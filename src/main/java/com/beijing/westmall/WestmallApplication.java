03bd8d5fce9409f0baf9c9f802fd35468fa816a8
Jokerblazes <zcd123@qq.com>
2018-05-14 14:20:25 +0800

    Initial commit

---
+++ src/main/java/com/beijing/westmall/WestmallApplication.java
@@ -0,0 +1,12 @@
+package com.beijing.westmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WestmallApplication.class, args);
+	}
+}
